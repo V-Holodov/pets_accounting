4a359be04906b14d22b1b697bca32eb6279d2855
V-Holodov <w.a.holodov@yandex.ru>
2021-12-16 16:22:52 +0500

    Add swager

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
+
+
+schema_view = get_schema_view(title='Pets API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
+    path("docs/", schema_view, name="docs"),
 ]
